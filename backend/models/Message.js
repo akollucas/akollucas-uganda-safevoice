0784fe3dff77c32a3691479b675ff2905ef18d0c
akollucas <akollucas@yahoo.com>
2026-04-29 16:56:44 +0300

    Create Message model for Sequelize

---
+++ backend/models/Message.js
@@ -0,0 +1,13 @@
+// backend/models/Message.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const Message = sequelize.define('Message', {
+  id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+  reportId: { type: DataTypes.STRING, allowNull: false, references: { model: 'Reports', key: 'id' } },
+  from: DataTypes.STRING,
+  text: DataTypes.TEXT,
+  readBy: { type: DataTypes.JSON, defaultValue: [] }
+});
+
+module.exports = Message;
